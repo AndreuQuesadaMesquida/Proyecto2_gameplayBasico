de932829cd0af3865ace8f3f4bd5bdcefbac7ed3
AndreuQuesadaMesquida <andreuquesadamesquida@gmail.com>
2023-10-31 12:55:20 +0100

    shooter

---
+++ Assets/Scripts/DestroyOutOfBouds.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBouds : MonoBehaviour
+{
+    [SerializeField] private float topBound = 25f;
+    [SerializeField] private float bottomBound = -10f;
+
+    private void Update()
+    {
+        // Comida
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+
+        // Animales
+        if (transform.position.z < bottomBound)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
